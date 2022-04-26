4bf468a38e636f65c23d6e99e4f31be2eba7e9f9
Jisu Jung <j_jisu@protonmail.com>
2022-04-26 16:30:56 +0900

    0426 1630. 설마 레포지토리 꼬인 건 아니겠지?

---
+++ src/main/java/com/github/spellhaven/RequestCont.java
@@ -0,0 +1,86 @@
+package com.github.spellhaven;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestCont
+ */
+@WebServlet("/requestex")
+public class RequestCont extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestCont() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		actionDo(request, response);
+	}
+	
+	protected void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setAttribute("id", "tiger");
+		request.setAttribute("pw", "12345");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/dispatcherJsp.jsp");
+		dispatcher.forward(request, response); // 위임 방법 1. RequestDispatcher 사용하기
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<!-- RequestCont.java의 L15를 봐라. @WebServlet("/requestex")라고 쓰여 있디. -->
+	<!-- index.jsp를 실행 -> RequestCont 실행됨 -> dispatcherJsp 실행됨. 구조 잘 봐 ㅋ -->
+
+	<a href = "requestex">RequestDispatcher 연습</a>
+	
+</body>
+</html>
